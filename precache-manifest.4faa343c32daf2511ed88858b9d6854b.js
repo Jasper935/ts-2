957f18b5f63e0a98d8b6fbe4ba45963981a28a2d
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-11-24 01:50:09 +0200

    Updates

---
+++ precache-manifest.4faa343c32daf2511ed88858b9d6854b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8049d2525af51c52ffe8",
+    "url": "/ts-2/static/css/main.fce0f6e1.chunk.css"
+  },
+  {
+    "revision": "8049d2525af51c52ffe8",
+    "url": "/ts-2/static/js/main.8049d252.chunk.js"
+  },
+  {
+    "revision": "0b52934cdd37158a8f82",
+    "url": "/ts-2/static/js/1.0b52934c.chunk.js"
+  },
+  {
+    "revision": "601360345a8a9bdcb6a4",
+    "url": "/ts-2/static/js/runtime~main.60136034.js"
+  },
+  {
+    "revision": "4fd44fc77af7945eeb2b0a700335570c",
+    "url": "/ts-2/index.html"
+  }
+];
